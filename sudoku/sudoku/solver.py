2bfb1bd7a217824973bb5bc812fda2c17a740204
sri850309-ux <sri850309@gmail.com>
2026-04-25 14:59:24 +0530

    Update solver.py

--- sudoku/sudoku/solver.py
+++ sudoku/sudoku/solver.py
@@ -1,1 +1,32 @@
+def is_valid(board, row, col, num):
+    if num in board[row]:
+        return False
 
+    for i in range(9):
+        if board[i][col] == num:
+            return False
+
+    start_row, start_col = 3 * (row // 3), 3 * (col // 3)
+
+    for i in range(3):
+        for j in range(3):
+            if board[start_row+i][start_col+j] == num:
+                return False
+
+    return True
+
+
+def solve_sudoku(board):
+    for row in range(9):
+        for col in range(9):
+            if board[row][col] == 0:
+                for num in range(1, 10):
+                    if is_valid(board, row, col, num):
+                        board[row][col] = num
+
+                        if solve_sudoku(board):
+                            return True
+
+                        board[row][col] = 0
+                return False
+    return True
